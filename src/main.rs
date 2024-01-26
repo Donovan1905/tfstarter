4826b266834026c4feb7f836e2a8ee23787fcd02
Donovan HOANG <dhoang@ippon.fr>
2024-01-26 18:13:42 +0100

    feat(template): copy template from template directory into current directory based on cli args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use clap::{Parser, Subcommand, Args};
+use std::{env, path::PathBuf};
+
+use clap::{Args, Parser, Subcommand};
 
 mod init;
+mod utils;
 
 #[derive(Parser)]
-#[command(author = "donovanhoang.pro@gmail.com", version, about, long_about = None)]
+#[command(version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
     cmd: Option<Commands>
@@ -16,20 +19,25 @@ enum Commands {
 
 #[derive(Args)]
 struct Generate {
-    #[arg(short, long, help = "Name of the template you want to use (based on folder name of the template)")]
-    template: Option<String> 
+    #[arg(short, long, help = "Name of the template you want to use (based on folder name of the template ~/.tfstarter/)")]
+    template: Option<String>
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let templates = init::init_app().expect("Failed to load templates");
+    let _templates = init::init_app().expect("Failed to load templates");
 
     match &cli.cmd {
         Some(Commands::Generate(template)) => {
             match template.template {
                 Some(ref _template) => {
-                    
+                    let home_dir = env::var("HOME").unwrap();
+                    let mut src = PathBuf::from(home_dir);
+                    src.push(".tfstarter/");
+                    src.push(_template);
+                    let dst = env::current_dir().unwrap();
+                    utils::copy_dir_all(src, dst).unwrap();
                     println!("{}", _template);
                 }
                 None => {
