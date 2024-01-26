7bbb55d4270ac23932213195c74e541723fcd5f1
donovan <donovanhoang.pro@gmail.com>
2024-01-26 22:42:45 +0100

    feat(list): add 'get' command to list all the available template

--- src/init.rs
+++ src/init.rs
@@ -1,6 +1,6 @@
 use std::{fs,path::{Path, PathBuf},env,io};
 
-pub fn init_app() -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
+pub fn init_app() -> Result<(), Box<dyn std::error::Error>> {
 
     let home_dir = env::var("HOME")?;
     let config_folder_path = Path::new(&home_dir).join(".tfstarter/");
@@ -8,10 +8,5 @@ pub fn init_app() -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
     // Create config directory if not exists
     fs::create_dir_all(config_folder_path.clone())?;
 
-
-    let entries = fs::read_dir(config_folder_path.clone())?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
-
-    Ok(entries)
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{env, path::PathBuf};
-
+use std::{env, path::PathBuf, fs, io};
 use clap::{Args, Parser, Subcommand};
+use colored::Colorize;
 
 mod init;
 mod utils;
@@ -14,6 +14,7 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    Get(Get),
     Generate(Generate)
 }
 
@@ -23,18 +24,26 @@ struct Generate {
     template: Option<String>
 }
 
+#[derive(Args)]
+struct Get {
+    #[arg(short, long, help = "Filter by tag (coming soon)")]
+    #[clap(default_value = "A")]
+    ttype: Option<String>
+}
+
 fn main() {
     let cli = Cli::parse();
 
-    let _templates = init::init_app().expect("Failed to load templates");
+    init::init_app().expect("Failed to load templates");
+
+    let home_dir = env::var("HOME").unwrap();
+    let mut src = PathBuf::from(home_dir);
+    src.push(".tfstarter/");
 
     match &cli.cmd {
         Some(Commands::Generate(template)) => {
             match template.template {
                 Some(ref _template) => {
-                    let home_dir = env::var("HOME").unwrap();
-                    let mut src = PathBuf::from(home_dir);
-                    src.push(".tfstarter/");
                     src.push(_template);
                     let dst = env::current_dir().unwrap();
                     utils::copy_dir_all(src, dst).unwrap();
@@ -44,8 +53,27 @@ fn main() {
                     println!("Error : missing template ref. See tfstarter generate -h");
                 }
             }
+
         }
-        None => {}
+        Some(Commands::Get(ttype)) => {
+            match ttype.ttype {
+                Some(ref _ttype) => {
+                    let entries = fs::read_dir(src).expect("Error : Failed to read templates folder")
+                        .map(|res| res.map(|e| e.path()))
+                        .collect::<Result<Vec<_>, io::Error>>().unwrap();
+
+                    for tpl in &entries {
+                        println!("> {}", tpl.into_iter().last().unwrap().to_str().unwrap().bold().purple())
+                    }
+                }
+                None => {
+                    println!("Error : missing type")
+                }
+            }
+        }
+        None => {
+            println!("Please provide argument to use tfstarter. See tfstarter -h for help")
+        }
     }
 
 }
